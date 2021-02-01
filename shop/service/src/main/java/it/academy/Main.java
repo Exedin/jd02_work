05514f08112d9ef578ffbef0a0197e590035292a
= <deathexedin@gmail.com>
2021-02-01 21:25:31 +0300

    classwork 01.02.2021 finished

---
+++ shop/service/src/main/java/it/academy/Main.java
@@ -0,0 +1,16 @@
+package it.academy;
+
+import it.academy.service.PromoService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext ac=
+                new ClassPathXmlApplicationContext("beans.xml");
+        Object promoService = ac.getBean("promoService");
+        System.out.println(promoService);
+
+        ((PromoService)promoService).printPromo();
+    }
+}
