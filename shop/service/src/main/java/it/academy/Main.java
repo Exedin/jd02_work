31662e5667983b2e6725fdfd214319d3edea0856
= <deathexedin@gmail.com>
2021-02-03 21:24:28 +0300

    classwork 03.02.2021 finished

--- shop/service/src/main/java/it/academy/Main.java
+++ shop/service/src/main/java/it/academy/Main.java
@@ -5,12 +5,14 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
-    public static void main(String[] args) {
-        ApplicationContext ac=
+    public static void main(String[] args) throws InterruptedException {
+        ApplicationContext ac =
                 new ClassPathXmlApplicationContext("beans.xml");
-        Object promoService = ac.getBean("promoService");
+        PromoService promoService = (PromoService)ac.getBean("promoService");
         System.out.println(promoService);
 
-        ((PromoService)promoService).printPromo();
+        promoService.printPromo();
+        ((ClassPathXmlApplicationContext)ac).close();
+
     }
 }
